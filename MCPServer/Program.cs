00e5c8a34bfcc5732a82c071a13c9754859067a5
santosh bambrule <santosh.bambrule@cognizant.com>
2026-03-05 09:54:50 +0100

    First code

---
+++ MCPServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MCPServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Logging.AddConsole(consoleLogOptions =>
+            {
+                consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+            });
+            builder.Services
+            .AddMcpServer()
+            .WithStdioServerTransport()
+            .WithToolsFromAssembly();
+
+            builder.Services.AddHttpClient();
+            builder.Services.AddSingleton<MonkeyService>();
+            
+            await builder.Build().RunAsync();
+        }
+    }
+}
